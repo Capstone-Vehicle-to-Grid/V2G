ca824ebe7f71706ebb4ee7865bfa283bb3b36e2c
Benjamin Worthington <benworthin@icloud.com>
2023-01-27 11:32:10 -0600

    Initial commit

---
+++ Vehicle to Grid/Vehicle_to_GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vehicle_to_GridApp.swift
+//  Vehicle to Grid
+//
+//  Created by Ben Worthington on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Vehicle_to_GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
