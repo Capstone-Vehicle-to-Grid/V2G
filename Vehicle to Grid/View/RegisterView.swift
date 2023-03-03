c4098c1a924ac8cfe71f42204079313a43a31689
Julia-Brixey <juliabrixey23@gmail.com>
2023-03-03 14:23:45 -0600

    Fixed "Register" button with link to add to database

--- Vehicle to Grid/View/RegisterView.swift
+++ Vehicle to Grid/View/RegisterView.swift
@@ -16,81 +16,57 @@ struct RegisterView: View {
   @State var nameText = ""
   @State var phoneText = ""
   @State var phoneEditing = false
-  
+  @StateObject var viewModel = UserViewModel()
+
+    
   var body: some View {
-    
-    // Causing issues
-    //    @StateObject var viewModel = UserViewModel()
-    //    @State var phoneEditing = false
-    //    @State var showAlert = false
-    
-    NavigationView {
       
-      VStack {
+      //@State var phoneEditing = false
+      //@State var showAlert = false
+      
+      NavigationView {
+      
+          VStack {
         
-        Text("Create new account")
-          .font(.largeTitle)
-          .foregroundColor(.blue)
-          .bold()
-          .padding()
-        
-        Text("Already registered? Login here.")
-          .foregroundColor(.blue)
-        
-        TextField("name", text: $name)
-          .padding()
-          .frame(width: 300, height: 50)
-          .background(Color.black.opacity(0.05))
-          .cornerRadius(10)
-        
-        TextField("Email", text: $emailRegister)
-          .padding()
-          .frame(width: 300, height: 50)
-          .background(Color.black.opacity(0.05))
-          .cornerRadius(10)
-        
-        SecureField("Password", text: $passwordRegister)
-          .padding()
-          .frame(width: 300, height: 50)
-          .background(Color.black.opacity(0.05))
-          .cornerRadius(10)
-        
+            Text("Create new account")
+              .font(.largeTitle)
+              .foregroundColor(.blue)
+              .bold()
+              .padding()
+
+            Text("Already registered? Login here.")
+              .foregroundColor(.blue)
+
+            TextField("name", text: $name)
+              .padding()
+              .frame(width: 300, height: 50)
+              .background(Color.black.opacity(0.05))
+              .cornerRadius(10)
+
+            TextField("Email", text: $emailRegister)
+              .padding()
+              .frame(width: 300, height: 50)
+              .background(Color.black.opacity(0.05))
+              .cornerRadius(10)
+
+            SecureField("Password", text: $passwordRegister)
+              .padding()
+              .frame(width: 300, height: 50)
+              .background(Color.black.opacity(0.05))
+              .cornerRadius(10)
+              
+            Button(action: registerUser) {Text("Register")}
+              .foregroundColor(.white)
+              .frame(width: 300, height: 50)
+              .background(Color.blue)
+              .cornerRadius(10)
+        }
       }
-      
-      //        Button(action: registerUser) {
-      //          Text("Register")
-      //        }
-      //        .foregroundColor(.white)
-      //        .frame(width: 300, height: 50)
-      //        .background(Color.blue)
-      //        .cornerRadius(10)
-      
-    }
   }
-  
-  //    func registerUser() {
-  //      let newUser = viewModel.register()
-  //    }
-  
+    func registerUser() {
+        _ = viewModel.register()
+    }
 }
-
-// Button to Sign Up
-//                Button(action: authenticateUser) {
-//                  Text("Sign Up")
-//                }
-//                .foregroundColor(.white)
-//                .frame(width: 300, height: 50)
-//                .background(Color.blue)
-//                .cornerRadius(10)
-//                .alert(isPresented: $showAlert) {
-//                  Alert(
-//
-//
-//
-//                  )
-//                }
-
-//            }
 
 struct RegisterView_Previews: PreviewProvider {
   static var previews: some View {
