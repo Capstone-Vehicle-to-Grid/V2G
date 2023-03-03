33cefa18e911ad7f97442280ba6eedf591dec230
Benjamin Worthington <benworthin@icloud.com>
2023-03-03 11:02:46 -0600

    Fixed errors and build issues

--- Vehicle to Grid/View/RegisterView.swift
+++ Vehicle to Grid/View/RegisterView.swift
@@ -19,88 +19,78 @@ struct RegisterView: View {
   
   var body: some View {
     
-    @StateObject var viewModel = UserViewModel()
-    @State private var name = ""
-    @State private var emailRegister = ""
-    @State private var passwordRegister = ""
-    @State var nameText = ""
-    @State var phoneText = ""
-    @State var phoneEditing = false
-    @State var showAlert = false
+    // Causing issues
+    //    @StateObject var viewModel = UserViewModel()
+    //    @State var phoneEditing = false
+    //    @State var showAlert = false
     
-    var body: some View {
+    NavigationView {
       
-      NavigationView {
+      VStack {
         
-        VStack {
-          
-          Text("Create new account")
-            .font(.largeTitle)
-            .foregroundColor(.blue)
-            .bold()
-            .padding()
-          
-          Text("Already registered? Login here.")
-            .foregroundColor(.blue)
-          
-          TextField("name", text: $name)
-            .padding()
-            .frame(width: 300, height: 50)
-            .background(Color.black.opacity(0.05))
-            .cornerRadius(10)
-          
-          TextField("Email", text: $emailRegister)
-            .padding()
-            .frame(width: 300, height: 50)
-            .background(Color.black.opacity(0.05))
-            .cornerRadius(10)
-          
-          SecureField("Password", text: $passwordRegister)
-            .padding()
-            .frame(width: 300, height: 50)
-            .background(Color.black.opacity(0.05))
-            .cornerRadius(10)
-          
-        }
+        Text("Create new account")
+          .font(.largeTitle)
+          .foregroundColor(.blue)
+          .bold()
+          .padding()
         
-        //        Button(action: registerUser) {
-        //          Text("Register")
-        //        }
-        //        .foregroundColor(.white)
-        //        .frame(width: 300, height: 50)
-        //        .background(Color.blue)
-        //        .cornerRadius(10)
+        Text("Already registered? Login here.")
+          .foregroundColor(.blue)
+        
+        TextField("name", text: $name)
+          .padding()
+          .frame(width: 300, height: 50)
+          .background(Color.black.opacity(0.05))
+          .cornerRadius(10)
+        
+        TextField("Email", text: $emailRegister)
+          .padding()
+          .frame(width: 300, height: 50)
+          .background(Color.black.opacity(0.05))
+          .cornerRadius(10)
+        
+        SecureField("Password", text: $passwordRegister)
+          .padding()
+          .frame(width: 300, height: 50)
+          .background(Color.black.opacity(0.05))
+          .cornerRadius(10)
         
       }
+      
+      //        Button(action: registerUser) {
+      //          Text("Register")
+      //        }
+      //        .foregroundColor(.white)
+      //        .frame(width: 300, height: 50)
+      //        .background(Color.blue)
+      //        .cornerRadius(10)
+      
     }
-    
-//    func registerUser() {
-//      let newUser = viewModel.register()
-//    }
-    
   }
   
-  // Button to Sign Up
-  //                Button(action: authenticateUser) {
-  //                  Text("Sign Up")
-  //                }
-  //                .foregroundColor(.white)
-  //                .frame(width: 300, height: 50)
-  //                .background(Color.blue)
-  //                .cornerRadius(10)
-  //                .alert(isPresented: $showAlert) {
-  //                  Alert(
-  //
-  //
-  //
-  //                  )
-  //                }
-  
-  //            }
+  //    func registerUser() {
+  //      let newUser = viewModel.register()
+  //    }
   
 }
 
-}
+// Button to Sign Up
+//                Button(action: authenticateUser) {
+//                  Text("Sign Up")
+//                }
+//                .foregroundColor(.white)
+//                .frame(width: 300, height: 50)
+//                .background(Color.blue)
+//                .cornerRadius(10)
+//                .alert(isPresented: $showAlert) {
+//                  Alert(
+//
+//
+//
+//                  )
+//                }
+
+//            }
 
 struct RegisterView_Previews: PreviewProvider {
   static var previews: some View {
