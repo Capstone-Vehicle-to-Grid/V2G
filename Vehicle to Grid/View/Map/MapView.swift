12e4f9d65383ccbc1889eeebb128c3b8fc4326e2
Joseph Taylor <84956758+git-onolix@users.noreply.github.com>
2023-03-10 20:49:55 -0600

    Created a crude filter for clustered markers (each pump was getting a marker)

--- Vehicle to Grid/View/Map/MapView.swift
+++ Vehicle to Grid/View/Map/MapView.swift
@@ -5,67 +5,88 @@
 //  Created by Chiyou Vang on 2/11/23.
 //
 
+import GoogleMaps
 import SwiftUI
 import UIKit
-import GoogleMaps
-
 
 struct MapView: View {
-    @ObservedObject var viewModel = MapViewModel()
+  @ObservedObject var viewModel = MapViewModel()
 
-    @State var markers: [GMSMarker] = []
-    
-    @State var zoomInCenter: Bool = false
-    @State var expandList: Bool = false
-    @State var selectedMarker: GMSMarker?
-    @State var yDragTranslation: CGFloat = 0
-    
-    var body: some View{
-        //let scrollViewHeight: CGFloat = 80
+  @State var markers: [GMSMarker] = []
 
-        GeometryReader { geometry in
-            ZStack(alignment: .top) {
-                // Map
-                if let userLocation = viewModel.userLocation {
-                    let camera = GMSCameraPosition.camera(withTarget: userLocation, zoom: 6.0)
-                    GoogleMapsViewRepresentable(camera: camera, markers: $markers)
-                } else {
-                    Text("Loading...")
-                }
-            }
-            .onReceive(viewModel.$stations) { stations in
-                markers = stations.map {
-                    let marker = GMSMarker(position: $0.coordinate)
-                    marker.title = $0.name
-                    print("Marker added at \($0.coordinate.latitude), \($0.coordinate.longitude) with name \($0.name)")
-                    return marker
-                }
-            }
-            
+  @State var zoomInCenter: Bool = false
+  @State var expandList: Bool = false
+  @State var selectedMarker: GMSMarker?
+  @State var yDragTranslation: CGFloat = 0
+
+  var body: some View {
+    //let scrollViewHeight: CGFloat = 80
+
+    GeometryReader { geometry in
+      ZStack(alignment: .top) {
+        // Map
+        if let userLocation = viewModel.userLocation {
+          let camera = GMSCameraPosition.camera(withTarget: userLocation, zoom: 6.0)
+          GoogleMapsViewRepresentable(camera: camera, markers: $markers)
+        } else {
+          Text("Loading...")
         }
+      }
+      .onReceive(viewModel.$stations) { stations in
+
+        //This is a crude filtering function and should probably be replaced with GoogleMapsUtils GMUClusterManager at some point in the near future
+
+        let minDistance = 100.0  // Minimum distance between markers in feet
+        var filteredStations = [ChargingStation]()
+
+        for station in stations {
+          let isTooClose =
+            filteredStations.first { otherStation in
+              let distance = CLLocation(
+                latitude: station.coordinate.latitude,
+                longitude: station.coordinate.longitude
+              ).distance(
+                from: CLLocation(
+                  latitude: otherStation.coordinate.latitude,
+                  longitude: otherStation.coordinate.longitude
+                ))
+
+              return distance < minDistance / 3.281  // Convert feet to meters
+            } != nil
+
+          if !isTooClose {
+            filteredStations.append(station)
+          }
+        }
+
+        markers = filteredStations.map {
+          let marker = GMSMarker(position: $0.coordinate)
+          marker.title = $0.name
+          return marker
+        }
+      }
+
     }
+  }
 }
 
 struct ChargingStationsList: View {
-    // Use a binding to the computed property in MapView
-    @Binding var markers: [GMSMarker]
-    var buttonAction: (GMSMarker) -> Void
-    var handleAction: () -> Void
+  @Binding var markers: [GMSMarker]
+  var buttonAction: (GMSMarker) -> Void
+  var handleAction: () -> Void
 
-    var body: some View {
-        GeometryReader { geometry in
-            VStack(spacing: 0) {
-                // …
-                // List of Charging Stations
-                List {
-                    ForEach(0..<self.markers.count) { id in
-                        let marker = self.markers[id]
-                        Button(action: { buttonAction(marker) }) {
-                            Text(marker.title ?? "")
-                        }
-                    }
-                }.frame(maxWidth: .infinity)
+  var body: some View {
+    GeometryReader { geometry in
+      VStack(spacing: 0) {
+        List {
+          ForEach(0..<self.markers.count) { id in
+            let marker = self.markers[id]
+            Button(action: { buttonAction(marker) }) {
+              Text(marker.title ?? "")
             }
-        }
+          }
+        }.frame(maxWidth: .infinity)
+      }
     }
+  }
 }
