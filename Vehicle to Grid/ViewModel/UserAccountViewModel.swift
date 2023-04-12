94541f3859967b2bb2cbfd4f6d1b4477a450bcf2
Julia-Brixey <juliabrixey23@gmail.com>
2023-04-12 00:45:24 -0500

    Update User Functionality

--- Vehicle to Grid/ViewModel/UserAccountViewModel.swift
+++ Vehicle to Grid/ViewModel/UserAccountViewModel.swift
@@ -11,7 +11,7 @@ import FirebaseAuth
 
 class UserAccountViewModel: ObservableObject {
     
-    @Published var user: User = User(username: "Test", password: "", userEmail: "Test2")
+    @Published var user: User = User(username: "", password: "", userEmail: "", vehicleMake: "", vehicleModel: "", vehicleVIN: "")
     private var userList = Firestore.firestore().collection("users")
     private var loggedInEmail: String = ""
     
@@ -34,8 +34,41 @@ class UserAccountViewModel: ObservableObject {
                     let data = document.data()
                     self.user.username = data["username"] as? String ?? ""
                     self.user.userEmail = data["userEmail"] as? String ?? ""
+                    self.user.password = data["password"] as? String ?? ""
+                    self.user.vehicleModel = data["vehicleModel"] as? String ?? ""
+                    self.user.vehicleMake = data["vehicleMake"] as? String ?? ""
+                    self.user.vehicleVIN = data["vehicleVIN"] as? String ?? ""
                 }
             }
     }
     
+    func updateInfo() {
+        let loggedInUser = Auth.auth().currentUser
+        if let loggedInUser = loggedInUser {
+            loggedInEmail = loggedInUser.email!
+        }
+        self.user.userEmail = self.user.userEmail.lowercased()
+        userList.whereField("userEmail", isEqualTo: loggedInEmail)
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error accessing database: \(err)")
+                } else {
+                    let document = querySnapshot!.documents.first
+                    document!.reference.updateData([
+                        "username": self.user.username,
+                        "userEmail": self.user.userEmail,
+                        "password": self.user.password,
+                        "vehicleMake": self.user.vehicleMake,
+                        "vehicleModel": self.user.vehicleModel,
+                        "vehicleVIN": self.user.vehicleVIN
+                    ])
+                }
+            }
+        Auth.auth().currentUser?.updateEmail(to: user.userEmail) { error in
+          print("Error: \(error)")
+        }
+        Auth.auth().currentUser?.updatePassword(to: user.password) { error in
+          print("Error: \(error)")
+        }
+    }
 }
